eb107dde156dc9973ccd2582c89ee23c27646590
gizemyuce <gizemyucee@gmail.com>
2022-06-06 21:09:53 +0200

    poly

--- fmnist_poly_multi_convnet.py
+++ fmnist_poly_multi_convnet.py
@@ -16,8 +16,48 @@ import wandb
 import os
 import numpy as np
 
+import math
+class PolynomialLoss(nn.Module):
+    """
+    Poly-tailed margin based losses that decay as v^{-\alpha} for \alpha > 0.
+    The theory here is that poly-tailed losses do not have max-margin behavior
+    and thus can work with importance weighting.
+    Poly-tailed losses are not defined at v=0 for v^{-\alpha}, and so there are
+    several variants that are supported via the [[type]] option
+    exp : f(v):= exp(-v+1) for v < 1, 1/v^\alpha otherwise
+    logit: f(v):= 1/log(2)log(1+exp(-v+1)) for v < 1, 1/v^\alpha else.
+    """
+
+    allowed_types = {"logit"}
+
+    def __init__(self, type: str, alpha: float, reduction: str = "none"):
+        super().__init__()
+        self.type = type
+        assert type in self.allowed_types
+        self.alpha = float(alpha)
+        assert reduction == "none"
+
+    def margin_fn(self, margin_vals: torch.Tensor):
+        indicator = margin_vals <= 1
+        inv_part = torch.pow(
+            margin_vals.abs(), -1 * self.alpha
+        )  # prevent exponentiating negative numbers by fractional powers
+        if self.type == "logit":
+            indicator = margin_vals <= 1
+            inv_part = torch.pow(margin_vals.abs(),-1*self.alpha)
+            logit_inner = -1 * margin_vals
+            logit_part = torch.nn.functional.softplus(logit_inner)/(math.log(1+math.exp(-1)))
+            scores = logit_part * indicator + inv_part * (~indicator)
+            return scores
+
+    def forward(self, logits, target):
+        target_sign = 2 * target - 1
+        margin_scores = (logits[:, 1] - logits[:, 0]) * target_sign
+        loss_values = self.margin_fn(margin_scores)
+        return loss_values
+
 hyperparameter_defaults = dict(
-    dropout = 0.5,
+    dropout = 0.6,
     channels_one = 16,
     channels_two = 32,
     batch_size = 100,
